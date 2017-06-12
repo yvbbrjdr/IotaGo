df5f1831fca212684fc16e0c0a11beb80a936e8c
yvbbrjdr <xyyqzy@hotmail.com>
2017-06-12 22:19:58 +0800

    loss, save, and load

--- PolicyNetwork.py
+++ PolicyNetwork.py
@@ -30,26 +30,43 @@ class PolicyNetwork(object):
         self.__y = tf.nn.relu(self.__y)
         self.__logy = tf.reshape(self.__y, [-1, size ** 2])
         self.__y = tf.nn.softmax(self.__logy)
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = self.__y_, logits = self.__logy))
-        self.__train_step = tf.train.GradientDescentOptimizer(0.9).minimize(cross_entropy)
+        self.__cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = self.__y_, logits = self.__logy))
+        self.__train_step = tf.train.GradientDescentOptimizer(0.01).minimize(self.__cross_entropy)
         self.__sess = tf.Session()
         self.__sess.run(tf.global_variables_initializer())
 
     def getSize(self):
         return self.__size
 
+    def save(self, filename):
+        if not isinstance(filename, str):
+            print "PolicyNetwork: save: error: invalid filename"
+            return False
+        tf.train.Saver().save(self.__sess, filename)
+        return True
+
+    def load(self, filename):
+        if not isinstance(filename, str):
+            print "PolicyNetwork: load: error: invalid filename"
+            return False
+        tf.train.Saver().restore(self.__sess, filename)
+        return True
+
     def inference(self, board):
         if not isinstance(board, gb) or board.getSize() != self.__size:
-            print "PolicyNetwork: eval: error: invalid board"
+            print "PolicyNetwork: inference: error: invalid board"
             return False
         return self.__sess.run(self.__y, {self.__x : [board.allFeatures()]}).reshape([self.__size, self.__size]).tolist()
 
-    def train(self, boards, moves):
+    def train(self, boards, moves, times = 1):
         if not isinstance(boards, list):
             print "PolicyNetwork: train: error: invalid boards"
             return False
         if not isinstance(moves, list):
             print "PolicyNetwork: train: error: invalid moves"
+            return False
+        if not isinstance(times, int) or times <= 0:
+            print "PolicyNetwork: train: error: invalid times"
             return False
         x = []
         y = []
@@ -65,8 +82,32 @@ class PolicyNetwork(object):
             tmp = [0] * (self.__size ** 2)
             tmp[move[0] * self.__size + move[1]] = 1
             y.append(tmp)
-        self.__sess.run(self.__train_step, {self.__x : x, self.__y_ : y})
+        for _ in range(times):
+            self.__sess.run(self.__train_step, {self.__x : x, self.__y_ : y})
         return True
+
+    def loss(self, boards, moves):
+        if not isinstance(boards, list):
+            print "PolicyNetwork: loss: error: invalid boards"
+            return False
+        if not isinstance(moves, list):
+            print "PolicyNetwork: loss: error: invalid moves"
+            return False
+        x = []
+        y = []
+        for board in boards:
+            if not isinstance(board, gb):
+                print "PolicyNetwork: loss: error: invalid board"
+                return False
+            x.append(board.allFeatures())
+        for move in moves:
+            if not isinstance(move, tuple) or len(move) != 2 or not isinstance(move[0], int) or not isinstance(move[1], int):
+                print "PolicyNetwork: loss: error: invalid move"
+                return False
+            tmp = [0] * (self.__size ** 2)
+            tmp[move[0] * self.__size + move[1]] = 1
+            y.append(tmp)
+        return self.__sess.run(self.__cross_entropy, {self.__x : x, self.__y_ : y})
 
     @staticmethod
     def weight_variable(shape):
@@ -85,8 +126,14 @@ def test():
     board2 = gb()
     board2.move(3, 3, gb.black)
     network = PolicyNetwork(board1.getSize())
-    for _ in range(100):
+    filename = raw_input('Filename: ')
+    if os.path.isfile(filename + '.meta'):
+        network.load(filename)
+    print "Loss:", network.loss([board1, board2], [(3, 3), (15, 15)])
+    for _ in range(1000):
         network.train([board1, board2], [(3, 3), (15, 15)])
+        network.save(filename)
+        print "Loss:", network.loss([board1, board2], [(3, 3), (15, 15)])
     rPrint([network.inference(board1), network.inference(board2)])
 
 if __name__ == '__main__':
