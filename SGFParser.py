4490c5890b72952a9bc66c3d6e655940990856b5
yvbbrjdr <xyyqzy@hotmail.com>
2017-06-12 21:33:15 +0800

    SGFParser

---
+++ SGFParser.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+from GoBoard import GoBoard as gb
+
+class SGFParser(object):
+
+    spotDic = {
+    'a' : 0, 'b' : 1, 'c' : 2, 'd' : 3, 'e' : 4,
+    'f' : 5, 'g' : 6, 'h' : 7, 'i' : 8, 'j' : 9,
+    'k' : 10, 'l' : 11, 'm' : 12, 'n' : 13, 'o' : 14,
+    'p' : 15, 'q' : 16, 'r' : 17, 's' : 18
+    }
+
+    def __init__(self, filename = None):
+        self.__opened = False
+        if filename != None:
+            self.open(filename)
+
+    def open(self, filename):
+        if not isinstance(filename, str):
+            print "SGFParser: open: error: invalid filename"
+            self.__opened = False
+            return False
+        with open(filename, "r") as f:
+            self.__moves = [s for s in f.read().split(';') if s[0] == 'B' or s[0] == 'W']
+        self.__opened = True
+        self.__index = 0
+        return True
+
+    def getNextMove(self):
+        while True:
+            if not self.hasNextMove():
+                return False
+            move = self.__moves[self.__index]
+            self.__index += 1
+            color = gb.black
+            if move[0] == 'W':
+                color = gb.white
+            if move[2] not in SGFParser.spotDic or move[3] not in SGFParser.spotDic:
+                continue
+            return (SGFParser.spotDic[move[3]], SGFParser.spotDic[move[2]], color)
+
+    def hasNextMove(self):
+        if not self.__opened:
+            return False
+        return self.__index < len(self.__moves)
+
+def test():
+    sgf = SGFParser(raw_input("Filename: "))
+    board = gb()
+    while sgf.hasNextMove():
+        move = sgf.getNextMove()
+        board.move(move[0], move[1], move[2])
+    board.printBoard()
+
+if __name__ == '__main__':
+    test()
