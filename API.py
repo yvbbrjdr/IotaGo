fa592ef8dee6cbf0127df71f61d6ea0c46c4d0cd
yvbbrjdr <xyyqzy@hotmail.com>
2017-06-15 15:07:42 +0800

    test

--- API.py
+++ API.py
@@ -4,6 +4,7 @@ from GoBoard import GoBoard as gb
 from SGFParser import SGFParser as sp
 from PolicyNetwork import PolicyNetwork as pn
 from copy import deepcopy
+import time
 
 def trainSGF():
     board = gb()
@@ -28,5 +29,31 @@ def trainSGF():
             print(network.lossAndAccuracy(boards[i * 50:i * 50 + 50], moves[i * 50:i * 50 + 50]))
     network.save(input('Save filename: '))
 
+def test():
+    board = gb()
+    network = pn()
+    network.load(input('Load filename: '))
+    while True:
+        color = board.getNextColor()
+        if 1 == 2:
+            board.printBoard()
+            x = input('x: ')
+            y = input('y: ')
+            board.move(int(x), int(y), color)
+        else:
+            y = network.inference(board)
+            maxp = y[0][0]
+            maxx = 0
+            maxy = 0
+            for i in range(network.getSize()):
+                for j in range(network.getSize()):
+                    if y[i][j] > maxp and board.isValidMove(i, j, color):
+                        maxp = y[i][j]
+                        maxx = i
+                        maxy = j
+            board.move(maxx, maxy, color)
+            board.printBoard()
+            time.sleep(1)
+
 if __name__ == '__main__':
-    trainSGF()
+    test()
