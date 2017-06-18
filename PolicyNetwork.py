83ea4075c05cde1270c2dedab9947be472801896
yvbbrjdr <xyyqzy@hotmail.com>
2017-06-18 17:58:42 +0800

    rebuild network with high level APIs

--- PolicyNetwork.py
+++ PolicyNetwork.py
@@ -10,6 +10,7 @@ class PolicyNetwork(object):
 
     filterCount = 256
     filterSize = 3
+    layerCount = 13
 
     def __init__(self, size = 19):
         if not isinstance(size, int) or size <= 0:
@@ -17,20 +18,14 @@ class PolicyNetwork(object):
         self.__size = size
         self.__x = tf.placeholder(tf.float32, shape = [None, size, size, gb.featureCount])
         self.__y_ = tf.placeholder(tf.float32, shape = [None, size ** 2])
-        self.__y = PolicyNetwork.conv2d(self.__x, PolicyNetwork.weightVariable([5, 5, gb.featureCount, PolicyNetwork.filterCount]))
-        self.__y += PolicyNetwork.biasVariable([PolicyNetwork.filterCount])
-        self.__y = tf.nn.elu(self.__y)
-        for _ in range(11):
-            self.__y = PolicyNetwork.conv2d(self.__y, PolicyNetwork.weightVariable([PolicyNetwork.filterSize, PolicyNetwork.filterSize, PolicyNetwork.filterCount, PolicyNetwork.filterCount]))
-            self.__y += PolicyNetwork.biasVariable([PolicyNetwork.filterCount])
-            self.__y = tf.nn.elu(self.__y)
-        self.__y = PolicyNetwork.conv2d(self.__y, PolicyNetwork.weightVariable([PolicyNetwork.filterSize, PolicyNetwork.filterSize, PolicyNetwork.filterCount, 1]))
-        self.__y += PolicyNetwork.biasVariable([1])
-        self.__y = tf.nn.elu(self.__y)
-        self.__logy = tf.reshape(self.__y, [-1, size ** 2])
-        self.__y = tf.nn.softmax(self.__logy)
-        self.__cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = self.__y_, logits = self.__logy))
-        self.__train_step = tf.train.MomentumOptimizer(0.01, 0.01).minimize(self.__cross_entropy)
+        self.__y = PolicyNetwork.conv2d(self.__x, PolicyNetwork.filterCount, 5)
+        for _ in range(PolicyNetwork.layerCount - 2):
+            self.__y = PolicyNetwork.conv2d(self.__y, PolicyNetwork.filterCount, PolicyNetwork.filterSize)
+        self.__y = PolicyNetwork.conv2d(self.__y, 1, PolicyNetwork.filterSize)
+        self.__rawY = tf.reshape(self.__y, [-1, size ** 2])
+        self.__y = tf.nn.softmax(self.__rawY)
+        self.__loss = tf.losses.softmax_cross_entropy(onehot_labels = self.__y_, logits = self.__rawY)
+        self.__train = tf.contrib.layers.optimize_loss(loss = self.__loss, global_step = tf.contrib.framework.get_global_step(), learning_rate = 0.001, optimizer = "SGD")
         correct_prediction = tf.equal(tf.argmax(self.__y, 1), tf.argmax(self.__y_, 1))
         self.__accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         self.__sess = tf.Session()
@@ -49,63 +44,42 @@ class PolicyNetwork(object):
             raise Exception('PolicyNetwork: load: error: invalid filename')
         tf.train.Saver().restore(self.__sess, filename)
 
-    def inference(self, board):
-        if not isinstance(board, gb) or board.getSize() != self.__size:
-            raise Exception('PolicyNetwork: inference: error: invalid board')
-        return self.__sess.run(self.__y, {self.__x : [board.allFeatures()]}).reshape([self.__size, self.__size]).tolist()
+    def getInput(self, boards, moves):
+        if not isinstance(boards, list):
+            raise Exception('PolicyNetwork: getInput: error: invalid boards')
+        if not isinstance(moves, list):
+            raise Exception('PolicyNetwork: getInput: error: invalid moves')
+        x = []
+        y = []
+        for board in boards:
+            if not isinstance(board, gb) or board.getSize() != self.__size:
+                raise Exception('PolicyNetwork: getInput: error: invalid board')
+            x.append(board.allFeatures())
+        for move in moves:
+            if not isinstance(move, tuple) or len(move) != 2 or not isinstance(move[0], int) or not 0 <= move[0] < self.__size or not isinstance(move[1], int) or not 0 <= move[1] < self.__size:
+                raise Exception('PolicyNetwork: getInput: error: invalid move')
+            tmp = [0] * (self.__size ** 2)
+            tmp[move[0] * self.__size + move[1]] = 1
+            y.append(tmp)
+        return (x, y)
+
+    def inference(self, boards):
+        return self.__sess.run(self.__y, {self.__x : self.getInput(boards, [])[0]}).reshape([-1, self.__size, self.__size]).tolist()
 
     def train(self, boards, moves, times = 1):
-        if not isinstance(boards, list):
-            raise Exception('PolicyNetwork: train: error: invalid boards')
-        if not isinstance(moves, list):
-            raise Exception('PolicyNetwork: train: error: invalid moves')
         if not isinstance(times, int) or times <= 0:
             raise Exception('PolicyNetwork: train: error: invalid times')
-        x = []
-        y = []
-        for board in boards:
-            if not isinstance(board, gb) or board.getSize() != self.__size:
-                raise Exception('PolicyNetwork: train: error: invalid board')
-            x.append(board.allFeatures())
-        for move in moves:
-            if not isinstance(move, tuple) or len(move) != 2 or not isinstance(move[0], int) or not isinstance(move[1], int):
-                raise Exception('PolicyNetwork: train: error: invalid move')
-            tmp = [0] * (self.__size ** 2)
-            tmp[move[0] * self.__size + move[1]] = 1
-            y.append(tmp)
+        inputs = self.getInput(boards, moves)
         for _ in range(times):
-            self.__sess.run(self.__train_step, {self.__x : x, self.__y_ : y})
+            self.__sess.run(self.__train, {self.__x : inputs[0], self.__y_ : inputs[1]})
 
     def lossAndAccuracy(self, boards, moves):
-        if not isinstance(boards, list):
-            raise Exception('PolicyNetwork: loss: error: invalid boards')
-        if not isinstance(moves, list):
-            raise Exception('PolicyNetwork: loss: error: invalid moves')
-        x = []
-        y = []
-        for board in boards:
-            if not isinstance(board, gb) or board.getSize() != self.__size:
-                raise Exception('PolicyNetwork: loss: error: invalid board')
-            x.append(board.allFeatures())
-        for move in moves:
-            if not isinstance(move, tuple) or len(move) != 2 or not isinstance(move[0], int) or not isinstance(move[1], int):
-                raise Exception('PolicyNetwork: loss: error: invalid move')
-            tmp = [0] * (self.__size ** 2)
-            tmp[move[0] * self.__size + move[1]] = 1
-            y.append(tmp)
-        return (self.__sess.run([self.__cross_entropy, self.__accuracy], {self.__x : x, self.__y_ : y}))
+        inputs = self.getInput(boards, moves)
+        return (self.__sess.run([self.__loss, self.__accuracy], {self.__x : inputs[0], self.__y_ : inputs[1]}))
 
     @staticmethod
-    def weightVariable(shape):
-        return tf.Variable(tf.truncated_normal(shape, stddev = 0.001))
-
-    @staticmethod
-    def biasVariable(shape):
-        return tf.Variable(tf.constant(0.1, shape = shape))
-
-    @staticmethod
-    def conv2d(x, W):
-        return tf.nn.conv2d(x, W, strides = [1, 1, 1, 1], padding = 'SAME')
+    def conv2d(inputs, filters, kernel_size):
+        return tf.layers.conv2d(inputs = inputs, filters = filters, kernel_size = [kernel_size, kernel_size], padding = 'same', activation = tf.nn.elu)
 
 def test():
     board1 = gb()
